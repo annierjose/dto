bef28677afc2982b5f9d60521010d320e82ca1be
jalfonso <jalfonso@sorint.it>
2018-09-24 00:30:27 +0200

    #3

--- src/main/java/it/annierjose/dto/runner/InsertNewTeam.java
+++ src/main/java/it/annierjose/dto/runner/InsertNewTeam.java
@@ -3,8 +3,11 @@ package it.annierjose.dto.runner;
 import it.annierjose.dto.model.Team;
 import it.annierjose.dto.model.dto.TeamCreateDTO;
 import it.annierjose.dto.repository.TeamRepository;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 
@@ -14,16 +17,18 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("it.gattifederico.customrepository")
 public class InsertNewTeam implements CommandLineRunner {
 
+    private static final Log logger = LogFactory.getLog(SpringApplication.class);
+
     @Autowired
     private TeamRepository teamRepository;
 
     @Override
     public void run(final String... args) throws Exception {
-        System.out.println("Insert new Team");
+        logger.info("Insert new Team");
         TeamCreateDTO teamCreateDTO = new TeamCreateDTO();
-        teamCreateDTO.setName("teamCreateDTO");
+        logger.info("teamCreateDTO");
         Team team = this.teamRepository.createTeam(teamCreateDTO);
-        System.out.println("Inserted team id: " + team.getId());
-        System.out.println("Inserted team name: " + team.getName());
+        logger.info("Inserted team id: " + team.getId());
+        logger.info("Inserted team name: " + team.getName());
     }
 }
--- src/main/java/it/annierjose/dto/runner/UpdateNewTeam.java
+++ src/main/java/it/annierjose/dto/runner/UpdateNewTeam.java
@@ -3,8 +3,11 @@ package it.annierjose.dto.runner;
 import it.annierjose.dto.model.dto.TeamUpdateDTO;
 import it.annierjose.dto.model.Team;
 import it.annierjose.dto.repository.TeamRepository;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -13,17 +16,19 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("it.gattifederico.customrepository")
 public class UpdateNewTeam implements CommandLineRunner {
 
+    private static final Log logger = LogFactory.getLog(SpringApplication.class);
+
     @Autowired
     private TeamRepository teamRepository;
 
     @Override
     public void run(final String... args) throws Exception {
-        System.out.println("Update new Team");
+        logger.info("Update new Team");
         TeamUpdateDTO teamUpdateDTO = new TeamUpdateDTO();
-        teamUpdateDTO.setName("teamUpdatedDTO");
+        logger.info("teamUpdatedDTO");
         teamUpdateDTO.setId(5L);
         Team team = this.teamRepository.updateTeam(teamUpdateDTO);
-        System.out.println("Updated team id: " + team.getId());
-        System.out.println("Updated team name: " + team.getName());
+        logger.info("Updated team id: " + team.getId());
+        logger.info("Updated team name: " + team.getName());
     }
 }
